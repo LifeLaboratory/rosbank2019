d0f2a6e75abe9a3fde247ef83e87cfe6228ef063
KateStich <ekate.enina@mail.ru>
2019-12-07 03:07:50 +0700

    Создана возможность добавления событий пользователя

--- app/route/route_list.py
+++ app/route/route_list.py
@@ -1,9 +1,15 @@
 from app.route.user.route import Auth, Register
 from app.route.profile.route import Profile
+from app.route.statistic.route import Statistic
+
+
+
 
 
 ROUTES = {
     '/register': Register,
     '/auth': Auth,
     '/profile/<int:id_user>': Profile,
+    '/api/statistic': Statistic,
+
 }
