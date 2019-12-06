a1b600df2fab0b0390d5ff4fa52befb269ee3e2f
Anton Chusovitin <antonion2011@yandex.ru>
2019-12-07 02:56:42 +0700

    * сменил роуты

--- app/route/route_list.py
+++ app/route/route_list.py
@@ -1,9 +1,8 @@
-from app.route.user.route import Auth, Register
+from app.route.user.route import Auth
 from app.route.profile.route import Profile
 
 
 ROUTES = {
-    '/register': Register,
-    '/auth': Auth,
-    '/profile/<int:id_user>': Profile,
+    'api/auth': Auth,
+    'api/profile/<int:id_user>': Profile,
 }
