e918cc2f9ae7be03624232155b704df4ff757d82
Anton Chusovitin <antonion2011@yandex.ru>
2019-12-07 18:14:34 +0700

    * Лучшая доброска для фикса

--- app/route/features/provider.py
+++ app/route/features/provider.py
@@ -6,9 +6,12 @@ class Provider:
     def get_feature_user(args):
         query = """
   select 
-    *
+    uf.*
+    , f.*
+    , us.name as user_name
   from "user_features" uf
   join "features" f using(id_features)
+  join users us using(id_user)
   where "id_user" = {id_user}
                   """
         return Sql.exec(query=query, args=args)
